cb455841df48b8fd8a26d536eb54cfcf9bc3db7f
Ibrahim Abdo <ibrahimporoo@gmail.com>
2023-06-11 02:05:05 +0300

    Uploading the index page

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,44 @@ const nextSlide = () => {
 }
 
 setInterval(nextSlide, 5000);
+
+// Swiper js library
+let swiper = new Swiper(".our-files", {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	loop: true,
+	centeredSlides: true,
+	autoplay: {
+		delay: 5500,
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".button-next",
+		prevEl: ".button-prev",
+	},
+});
+// var swiper = new Swiper(".our-files", {
+// 	spaceBetween: 20,
+// 	centeredSlides: true,
+// 	autoplay: {
+// 		delay: 200,
+// 		disableOnInteraction: false
+// 	},
+// 	breakpoints: {
+// 		0: {
+// 			slidesPerView: 0,
+// 		},
+// 		500: {
+// 			slidesPerView: 2,
+// 		},
+// 		768: {
+// 			slidesPerView: 2,
+// 		},
+// 		1020: {
+// 			slidesPerView: 3,
+// 		}
+// 	}
+// });
