30ecdec79d42f884819654fb1296cf78807b3b5b
Ibrahim Abdo <ibrahimporoo@gmail.com>
2023-06-14 02:08:21 +0300

    Uploading the Clients Sections with some updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,39 @@ let swiper = new Swiper(".our-files", {
 		prevEl: ".button-prev",
 	},
 });
+
+new Swiper('.clients-slider', {
+	speed: 400,
+	loop: true,
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false
+	},
+	slidesPerView: 'auto',
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 2,
+			spaceBetween: 40
+		},
+		480: {
+			slidesPerView: 3,
+			spaceBetween: 60
+		},
+		640: {
+			slidesPerView: 4,
+			spaceBetween: 80
+		},
+		992: {
+			slidesPerView: 6,
+			spaceBetween: 120
+		}
+	}
+});
 // var swiper = new Swiper(".our-files", {
 // 	spaceBetween: 20,
 // 	centeredSlides: true,
